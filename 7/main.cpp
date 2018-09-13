8db33a5990f5c701a2155df625aa0d8d85a8d761
mybelief321 <ylxbwws@sina.com>
2018-09-13 15:56:42 +0800

    add test7

---
+++ 7/main.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"config.h"
+
+#ifdef HAVE_ADD
+
+#else
+#include "math/MathAdd.h"
+#endif
+
+int main(int argc,char *argv[])
+{
+	printf("%s Version %d.%d\n",argv[0],APP_VERSION_MAJOR,APP_VERSION_MINOR);
+	if(argc < 3){
+		printf("Usage:%s num1 num2\n",argv[0]);
+		return 1;
+	}
+
+	double a = atof(argv[1]);
+	double b = atof(argv[2]);
+#ifdef HAVE_ADD
+	double sum = a*b;
+	printf("%g * %g = %g\n",a,b,sum);
+#else
+	double sum = add(a,b);
+	printf("%g + %g = %g\n",a,b,sum);
+#endif
+	return 0;
+}
